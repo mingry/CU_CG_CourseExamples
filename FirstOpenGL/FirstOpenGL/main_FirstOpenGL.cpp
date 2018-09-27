fedd6f20696cc12ae5dff296c94df2ffd96f0706
myung geol choi <myunggeol@gmail.com>
2018-09-27 10:59:37 +0900

    update comments

--- FirstOpenGL/FirstOpenGL/main_FirstOpenGL.cpp
+++ FirstOpenGL/FirstOpenGL/main_FirstOpenGL.cpp
@@ -7,27 +7,102 @@ GLuint g_window_w = 500;
 GLuint g_window_h = 500;
 
 
+//////////////////////////////////////////////////////////////////////
+//// Define Mesh Model
+//////////////////////////////////////////////////////////////////////
 const int g_num_vertices = 4;
 const int g_num_triangles = 2;
 
+GLfloat m_positions[g_num_vertices * 4] =
+{ 0.f,  0.f,  0.f, 1.f
+, 0.5f, 0.f,  0.f, 1.f
+, 0.5f, 0.5f, 0.f, 1.f
+, 0.f,  0.5f, 0.f, 1.f
+};
 
+GLfloat m_normals[g_num_vertices * 4] =
+{ 0.f, 0.f, 1.f, 0.f
+, 0.f, 0.f, 1.f, 0.f
+, 0.f, 0.f, 1.f, 0.f
+, 0.f, 0.f, 1.f, 0.f
+};
+
+GLfloat m_colors[g_num_vertices * 4] =
+{ 1.f, 0.f, 0.f, 1.f
+, 0.f, 1.f, 0.f, 1.f
+, 0.f, 0.f, 1.f, 1.f
+, 1.f, 1.f, 0.f, 1.f
+};
+
+GLuint m_indices[g_num_triangles * 3] =
+{
+	0, 1, 2,
+	0, 2, 3
+};
+
+
+//////////////////////////////////////////////////////////////////////
+//// Define Shader Programs
+//////////////////////////////////////////////////////////////////////
+
+// vertex shader source code
+const GLchar * v_shader_source =
+R"(
+	#version 330
+
+	layout (location=0) in vec4 vs_position;
+	layout (location=1) in vec4 vs_normal;
+	layout (location=2) in vec4 vs_color;
+
+	//uniform mat4 projection_matrix;
+	//uniform mat4 modelview_matrix;
+
+	out vec4 fs_color;
+
+	void main()
+	{
+		gl_Position = vs_position; //projection_matrix * modelview_matrix * vs_position;
+		fs_color = vs_color;
+	}
+		
+)";
+
+// fragament shader source code
+const GLchar * f_shader_code =
+R"(
+	#version 330
+
+	in vec4 fs_color;
+	
+	layout (location = 0) out vec4 color;
+
+	void main()
+	{
+		color = fs_color;
+	}
+		
+)";
+
+//////////////////////////////////////////////////////////////////////
+//// Declare VAO, VBO variables
+//////////////////////////////////////////////////////////////////////
 GLuint g_vao_id;
 GLuint g_vbo_position_id;
 GLuint g_vbo_normal_id;
 GLuint g_vbo_color_id;
 GLuint g_index_buffer_id;
 
-
 // Index Buffer Object
 GLuint index_buffer_id;
 
+
+//////////////////////////////////////////////////////////////////////
+//// Declare callback functions
+//////////////////////////////////////////////////////////////////////
 void Reshape(int w, int h);
 void Display();
-void Keyboard(unsigned char key, int x, int y);
-void SpeicalKeyboard(int key, int x, int y);
-void Mouse(int button, int state, int x, int y);
-void MouseMotion(int x, int y);
 void Timer(int value);
+
 
 
 
@@ -35,44 +110,42 @@ int main(int argc, char** argv)
 {
 
 
-	////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
 	//// 1. freeglut 초기화, 원도우 생성하기. 
 	////    Ref: https://en.wikibooks.org/wiki/OpenGL_Programming/Installation/GLUT
-	///////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
 
-	// 1.1. 최기화
+	//// 1.1. 최기화
 	glutInit(&argc, argv);
 	
-	// 1.2. 윈도우 모드 설정. 
+	//// 1.2. 윈도우 모드 설정. 
 	// 더블버퍼링, RGB 프레임버퍼, Depth 버퍼를 사용한다.
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
-	// 1.3. 윈도우 사이즈 설정 (가로, 세로 픽셀 수).
+	//// 1.3. 윈도우 사이즈 설정 (가로, 세로 픽셀 수).
 	glutInitWindowSize(g_window_w, g_window_h);
 
-	// 1.4. OpenGL Context와 연결된 윈도우 생성.
+	//// 1.4. OpenGL Context와 연결된 윈도우 생성.
 	glutCreateWindow("Computer Graphics");
 	
-	// 1.5. Callback functions 
+	//// 1.5. Callback functions 
 	// Ref: http://freeglut.sourceforge.net/docs/api.php#GlobalCallback
 	//      http://freeglut.sourceforge.net/docs/api.php#WindowCallback
 	//      https://www.opengl.org/resources/libraries/glut/spec3/node45.html
 	glutReshapeFunc(Reshape);
 	glutDisplayFunc(Display);
-	glutKeyboardFunc(Keyboard);
-	glutSpecialFunc(SpeicalKeyboard);
-	glutMouseFunc(Mouse);
-	glutMotionFunc(MouseMotion);
 	glutTimerFunc((unsigned int)(1000 /60), Timer, 0);
 
 
 
 
 
-	///////////////////////////////////////////////////////////////////////
-	//// Initialize GLEW
-	glewExperimental = TRUE;
-	if (glewInit() != GLEW_OK)
+	//////////////////////////////////////////////////////////////////////////////////////
+	//// 2. GLEW 초기화. 
+	////    Ref: http://glew.sourceforge.net/basic.html
+	//////////////////////////////////////////////////////////////////////////////////////
+	glewExperimental = GL_TRUE;
+	if ( glewInit() != GLEW_OK )
 	{
 		std::cerr << "Unable to initialize GLEW ... exiting" << std::endl;
 		exit(EXIT_FAILURE);
@@ -82,39 +155,26 @@ int main(int argc, char** argv)
 		std::cout << "GLEW OK\n";
 	}
 
+
 	
 
-	///////////////////////////////////////////////////////////////////////
-	//// Vertex shader program 
 
-	// create vertex shader and obejct the id.
+	//////////////////////////////////////////////////////////////////////////////////////
+	//// 3. Shader Programs 등록
+	////    Ref: https://www.khronos.org/opengl/wiki/Shader_Compilation
+	//////////////////////////////////////////////////////////////////////////////////////
+
+	//// Vertex shader program
+	//// 3.1. Vertex shader 객체 생성.
 	GLuint v_shader_id = glCreateShader(GL_VERTEX_SHADER);
 
-	// vertex shader source code
-	const GLchar * v_shader_source = 
-		R"(
-			#version 330
-
-			layout (location=0) in vec4 vs_position;
-			layout (location=1) in vec4 vs_normal;
-			layout (location=2) in vec4 vs_color;
-
-			//uniform mat4 projection_matrix;
-			//uniform mat4 modelview_matrix;
-
-			out vec4 fs_color;
-
-			void main()
-			{
-				gl_Position = vs_position; //projection_matrix * modelview_matrix * vs_position;
-				fs_color = vs_color;
-			}
-		
-		)";
-
+	//// 3.2. Vertex shader 소스코드 입력.
 	glShaderSource(v_shader_id, 1, &v_shader_source, NULL);
+
+	//// 3.3. Vertex shader 소스코드 컴파일.
 	glCompileShader(v_shader_id);
 
+	//// 3.4. 컴파일 에러 발생시 에러 메세지 출력.
 	{
 		GLint compiled;
 		glGetShaderiv(v_shader_id, GL_COMPILE_STATUS, &compiled);
@@ -133,31 +193,17 @@ int main(int argc, char** argv)
 	}
 
 
-	///////////////////////////////////////////////////////////////////////
 	//// Fragment shader program 
-
-	// create vertex shader and obejct the id.
+	//// 3.5. Fragment shader 객체 생성.
 	GLuint f_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
 
-	// fragament shader source code
-	const GLchar * f_shader_code =
-		R"(
-			#version 330
-
-			in vec4 fs_color;
-	
-			layout (location = 0) out vec4 color;
-
-			void main()
-			{
-				color = fs_color;
-			}
-		
-		)";
-
+	//// 3.6. Fragment shader 소스코드 입력.
 	glShaderSource(f_shader_id, 1, &f_shader_code, NULL);
+
+	//// 3.7. Fragment shader 소스코드 컴파일.
 	glCompileShader(f_shader_id);
 
+	//// 3.8. 컴파일 에러 발생시 에러 메세지 출력.
 	{
 		GLint compiled;
 		glGetShaderiv(f_shader_id, GL_COMPILE_STATUS, &compiled);
@@ -175,95 +221,87 @@ int main(int argc, char** argv)
 		}
 	}
 
-
-	///////////////////////////////////////////////////////////////////////
 	//// Shader Program (vertex shader + fragment shader)
-
-	// Create shader program and obtain the id. 
+	//// 3.9. Shader Program 객채 생성.
 	GLuint s_program_id = glCreateProgram();
 
-	// Attache vertex and fragment shaders to the program
+	//// 3.10. Vertex shader와 Fragment shader를 Shader Program 객체에 등록.
 	glAttachShader(s_program_id, v_shader_id);
 	glAttachShader(s_program_id, f_shader_id);
 
+	//// 3.11. Shader Program 링크
 	glLinkProgram(s_program_id);
+
+	//// 3.12. Shader Program 사용시작
 	glUseProgram(s_program_id);
 
 
 
 
 
-	///////////////////////////////////////////////////////////////////////
-	//// Initialize OpenGL
+
+	////////////////////////////////////////////////////////////////////////////////////
+	//// 4. OpenGL 설정
+	//////////////////////////////////////////////////////////////////////////////////////
 
 	glViewport(0, 0, (GLsizei)g_window_w, (GLsizei)g_window_h);
-	
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
+
 	
 
-	///////////////////////////////////////////////////////////////////////
-	//// Mesh Model
-	GLfloat m_positions[g_num_vertices * 4] = 
-	{     0.f,  0.f,  0.f, 1.f
-		, 0.5f, 0.f,  0.f, 1.f
-		, 0.5f, 0.5f, 0.f, 1.f
-		, 0.f,  0.5f, 0.f, 1.f
-	};
+	
 
-	GLfloat m_normals[g_num_vertices * 4] =
-	{     0.f, 0.f, 1.f, 0.f
-		, 0.f, 0.f, 1.f, 0.f
-		, 0.f, 0.f, 1.f, 0.f
-		, 0.f, 0.f, 1.f, 0.f
-	};
+	
+	////////////////////////////////////////////////////////////////////////////////////
+	//// 5. VAO, VBO 생성
+	////    Ref: https://www.khronos.org/opengl/wiki/Vertex_Specification#Vertex_Array_Object
+	///////////////////////////////////////////////////////////////////////////////////
 
-	GLfloat m_colors[g_num_vertices * 4] =
-	{     1.f, 0.f, 0.f, 1.f
-		, 0.f, 1.f, 0.f, 1.f
-		, 0.f, 0.f, 1.f, 1.f
-		, 1.f, 1.f, 0.f, 1.f
-	};
-
-
-	GLuint m_indices[g_num_triangles * 3] =
-	{
-		0, 1, 2,
-		0, 2, 3
-	};
-
-
-	///////////////////////////////////////////////////////////////////////
-	//// Vertex Array Object
-	// VAO를 생성하고 자동으로 부여된 이름을 g_two_tri_vao에 담는다.
+	//// 5.1. VAO 객체 생성 및 바인딩
 	glGenVertexArrays(1, &g_vao_id);
 	glBindVertexArray(g_vao_id);
 
-
-	//// VBO for positions
-	// 꼭지점(vertex)의 좌표값을 저장하기 위한 VBO를 생성하고 자동으로 부여된 VBO의 이름을 g_two_tri_vbo_pos에 담는다.
+	
+	//// 5.2. vertex positions 저장을 위한 VBO 생성 및 바인딩.
 	glGenBuffers(1, &g_vbo_position_id);
 	glBindBuffer(GL_ARRAY_BUFFER, g_vbo_position_id);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 4 * g_num_vertices, m_positions, GL_STATIC_DRAW);	
+
+	//// 5.3. vertex positions 데이터 입력.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 4 * g_num_vertices, m_positions, GL_STATIC_DRAW);
+
+	//// 5.4. 현재 바인딩되어있는 VBO를 shader program과 연결
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 
+
+	//// 5.5. vertex normal vectors 저장을 위한 VBO 생성 및 바인딩.
 	glGenBuffers(1, &g_vbo_normal_id);
 	glBindBuffer(GL_ARRAY_BUFFER, g_vbo_normal_id);
+
+	//// 5.6. vertex positions 데이터 입력.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 4 * g_num_vertices, m_normals, GL_STATIC_DRAW);
+
+	//// 5.7. 현재 바인딩되어있는 VBO를 shader program과 연결
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(1);
 
+
+	//// 5.8. vertex colors 저장을 위한 VBO 생성 및 바인딩.
 	glGenBuffers(1, &g_vbo_color_id);
 	glBindBuffer(GL_ARRAY_BUFFER, g_vbo_color_id);
+
+	//// 5.9. vertex positions 데이터 입력.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 4 * g_num_vertices, m_colors, GL_STATIC_DRAW);
+
+	//// 5.10. 현재 바인딩되어있는 VBO를 shader program과 연결
 	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(2);
 
 
-	// Index Buffer Object
+	//// 5.11. Index Buffer 객체 생성 및 데이터 입력
 	glGenBuffers(1, &g_index_buffer_id);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_index_buffer_id);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * 3 * g_num_triangles, m_indices, GL_STATIC_DRAW);
@@ -271,8 +309,7 @@ int main(int argc, char** argv)
 	
 
 
-	// 1.6. freeglut 윈도우 이벤트 처리 시작. 윈도우가 닫힐때까지 후한루프 실행.
-	//      Enter event loop
+	//// 1.6. freeglut 윈도우 이벤트 처리 시작. 윈도우가 닫힐때까지 후한루프 실행.
 	glutMainLoop();
 
 	return 0;
@@ -281,20 +318,31 @@ int main(int argc, char** argv)
 
 
 /**
-Display: 그림을 그리기 위해 호출되는 callback 함수.
-화면에 그림이 새로 그려질 필요가 있을 때마다 자동으로 반복해서 여러 번 호출된다.
-(예. 다른 윈도우에 의해 화면의 일부분이 가려졌다가 다시 나타나는 경우.)
-반복적으로 호출된다는 것을 명심하고, 불필요한 내용은 절대 이곳에 넣어서는 안된다.
-예를 들어, VAO나 VBO를 생성하는 코드를 이곳에 넣어서는 안된다. 이 곳에는 이미 생성된
-VAO와 VBO를 사용해서 그림을 그리는 코드만 포함되어야 한다.
+Display: 윈도우 화면이 업데이트 될 필요가 있을 때 호출되는 callback 함수.
 
-이 예제의 경우에는 g_draw_mode가 변함에 따라 다른 그림이 나타나도록 하였다.
+윈도우 상에 최종 결과를 렌더링 하는 코드는 이 함수 내에 구현해야한다.
+원도우가 처음 열릴 때, 윈도우 크기가 바뀔 때, 다른 윈도우에 의해 화면의 일부
+또는 전체가 가려졌다가 다시 나타날 때 등 시스템이 해당 윈도우 내의 그림에 대한
+업데이트가 필요하다고 판단하는 경우 자동으로 호출된다.
+강제 호출이 필요한 경우에는 glutPostRedisplay() 함수를 호출하면된다.
+
+이 함수는 불시에 빈번하게 호출된다는 것을 명심하고, 윈도우 상태 변화와 무관한
+1회성 코드는 가능한한 이 함수 밖에 구현해야한다. 특히 메모리 할당, VAO, VBO 생성
+등의 하드웨어 점유를 시도하는 코드는 특별한 이유가 없다면 절대 이 함수에 포함시키면
+안된다. 예를 들어, 메시 모델을 정의하고 VAO, VBO를 설정하는 부분은 최초 1회만
+실행하면되므로 main() 함수 등 외부에 구현해야한다. 정의된 메시 모델을 프레임 버퍼에
+그리도록 지시하는 코드만 이 함수에 구현하면 된다.
+
+만일, 이 함수 내에서 동적 메모리 할당을 해야하는 경우가 있다면 해당 메모리는 반드시
+이 함수가 끝나기 전에 해제 해야한다.
+
+ref: https://www.opengl.org/resources/libraries/glut/spec3/node46.html#SECTION00081000000000000000
 */
 void Display()
 {
 	// 전체 화면을 지운다.
 	// glClear는 Display 함수 가장 윗 부분에서 한 번만 호출되어야한다.
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
@@ -303,7 +351,7 @@ void Display()
 	glDrawElements(GL_TRIANGLES, g_num_triangles * 3, GL_UNSIGNED_INT, 0);
 
 	
-	// 더블버퍼링 수행을 위한 함수 호출.
+	// flipping the double buffers
 	// glutSwapBuffers는 항상 Display 함수 가장 아래 부분에서 한 번만 호출되어야한다.
 	glutSwapBuffers();
 }
@@ -311,18 +359,19 @@ void Display()
 
 
 /**
-Timer: 애니메이션을 위해 주기적으로 호출되는 함수.
-이 프로그램에서는 1초에 60번 호출되도록 설계한다.
-흘러간 시간을 업데이트하고 이를 바탕으로 선풍기 모터의 각도를 새로 계산한다.
+Timer: 지정된 시간 후에 자동으로 호출되는 callback 함수.
+ref: https://www.opengl.org/resources/libraries/glut/spec3/node64.html#SECTION000819000000000000000
 */
 void Timer(int value)
 {
 
 	// glutPostRedisplay는 가능한 빠른 시간 안에 전체 그림을 다시 그릴 것을 시스템에 요청한다.
-	// 결과적으로 Display() 함수를 호출하게 된다.
+	// 결과적으로 Display() 함수가 호출 된다.
 	glutPostRedisplay();
 
 	// 1/60 초 후에 Timer 함수가 다시 호출되로록 한다.
+	// Timer 함수 가 동일한 시간 간격으로 반복 호출되게하여,
+	// 애니메이션 효과를 표현할 수 있다
 	glutTimerFunc((unsigned int)(1000 / 60), Timer, 0);
 }
 
@@ -330,20 +379,15 @@ void Timer(int value)
 
 /**
 Reshape: 윈도우의 크기가 조정될 때마다 자동으로 호출되는 callback 함수.
-w와 h는 각각 조정된 윈도우의 가로 크기와 세로 크기를 나타낸다 (픽셀 단위).
-화면 크기가 변화하면, Viewport의 크기, 카메라의 가로세로 비율 등을 다시 설정해야한다.
+
+@param w, h는 각각 조정된 윈도우의 가로 크기와 세로 크기 (픽셀 단위).
+ref: https://www.opengl.org/resources/libraries/glut/spec3/node48.html#SECTION00083000000000000000
 */
 void Reshape(int w, int h)
 {
 	//  w : window width   h : window height
 	g_window_w = w;
 	g_window_h = h;
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
-	//  w : window width   h : window height
-	//glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 
 	glutPostRedisplay();
 }
@@ -351,129 +395,6 @@ void Reshape(int w, int h)
 
 
 
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////////
-/// Keyboard and Mouse Input
-
-
-
-/**
-Mouse: 마우스 버튼이 입력될 때마다 자동으로 호출되는 함수.
-파라메터의 의미는 다음과 같다.
-1. button: 사용된 버튼의 종류
-GLUT_LEFT_BUTTON - 왼쪽 버튼
-GLUT_RIGHT_BUTTON - 오른쪽 버튼
-GLUT_MIDDLE_BUTTON - 가운데 버튼 (휠이 눌러졌을 때)
-3 - 마우스 휠 (휠이 위로 돌아 갔음).
-4 - 마우스 휠 (휠이 아래로 돌아 갔음).
-2. state: 조작 상태
-GLUT_DOWN - 눌러 졌음
-GLUT_UP - 놓여졌음
-3. x,y: 조작이 일어났을 때, 마우스 포인터의 좌표값.
-*/
-void Mouse(int button, int state, int x, int y)
-{
-
-
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-	{
-		std::cout << "mouse left button pushed : (" << x << ", " << y << ")" << std::endl;
-	}
-	else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
-	{
-		std::cout << "mouse left button released : (" << x << ", " << y << ")" << std::endl;
-	}
-	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-	{
-		std::cout << "mouse right button pushed : (" << x << ", " << y << ")" << std::endl;
-	}
-	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP)
-	{
-		std::cout << "mouse right button released : (" << x << ", " << y << ")" << std::endl;
-	}
-	else if (button == 3)
-	{
-		std::cout << "mouse wheel up" << std::endl;
-	}
-	else if (button == 4)
-	{
-		std::cout << "mouse wheel down" << std::endl;
-	}
-
-}
-
-
-
-
-
-/**
-MouseMotion: 마우스 포인터가 움직일 때마다 자동으로 호출되는 함수.
-파라메터 x,y는 현재 마우스 포인터의 좌표값을 나타낸다.
-*/
-void MouseMotion(int x, int y)
-{
-	std::cout << "mouse dragged : (" << x << ", " << y << ")" << std::endl;
-
-}
-
-
-
-/**
-Keyboard: 키보드 입력이 있을 때마다 자동으로 호출되는 함수.
-파라메터 key 는 눌려진 키보드의 문자값을 나타낸다.
-파라메터 x,y는 현재 마우스 포인터의 좌표값을 나타낸다.
-
-이 예제에서는 키보드 '1', '2', '3', '4' 가 눌려지면 g_draw_mode를 변경한다.
-*/
-void Keyboard(unsigned char key, int x, int y)
-{
-	std::cout << "key '" << key << "' pushed : (" << x << ", " << y << ")" << std::endl;
-	
-
-
-	// glutPostRedisplay는 가능한 빠른 시간 안에 전체 그림을 다시 그릴 것을 시스템에 요청한다.
-	// 결과적으로 Display() 함수를 호출하게 된다.
-	glutPostRedisplay();
-}
-
-
-
-
-/**
-SpeicalKeyboard: 문자값으로 표현하기 어려운 키보드 입력이 있을 때마다 자동으로 호출되는 함수.
-파라메터 key 는 눌려진 키보드를 나타내는 매크로 값 (freeglut_std.h 참고).
-파라메터 x,y는 현재 마우스 포인터의 좌표값.
-*/
-void SpeicalKeyboard(int key, int x, int y)
-{
-	switch (key)
-	{
-		std::cout << key << std::endl;
-		/* fill here */
-	case GLUT_KEY_DOWN:
-		std::cout << "down" << std::endl;
-		break;
-
-	case GLUT_KEY_UP:
-		std::cout << "up" << std::endl;
-		break;
-
-	case GLUT_KEY_LEFT:
-		std::cout << "left" << std::endl;
-		break;
-
-	case GLUT_KEY_RIGHT:
-		std::cout << "right" << std::endl;
-		break;
-
-	}
-
-	glutPostRedisplay();
-}
 
 
 
